a9be6e5f4918fa1122a97a946e368a44c8c4bebb
den-rgb <den02_m@yahoo.com>
2023-10-26 11:43:22 +0100

    OCM-4513 | fix: Fixed kms arn regex pattern

--- pkg/resource/validations/kms_arn_regex_validation_test.go
+++ pkg/resource/validations/kms_arn_regex_validation_test.go
@@ -1,54 +1,52 @@
 package validations
 
 import (
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Validations", func() {
 	Describe("validateKMSKeyARN", func() {
-		var (
-			kmsKeyARN  string
-		)
+		Context("empty kmsKeyARN", func() {
+			When("kmsKeyARN is nil", func() {
+				It("should not return an error", func() {
+					err := ValidateKMSKeyARN(nil)
+					Expect(err).ToNot(HaveOccurred())
+				})
+			})
 
-		BeforeEach(func() {
-			kmsKeyARN = ""
-		})
-
-		Context("when kmsKeyARN is nil", func() {
-            It("should not return an error", func() {
-                err := ValidateKMSKeyARN(nil)
-                Expect(err).ToNot(HaveOccurred())
-            })
-        })
-
-		Context("when kmsKeyARN is empty", func() {
-			It("should not return an error", func() {
-				err := ValidateKMSKeyARN(&kmsKeyARN)
-				Expect(err).ToNot(HaveOccurred())
+			When("kmsKeyARN is empty", func() {
+				It("should not return an error", func() {
+					emptyKmsKeyARN := ""
+					err := ValidateKMSKeyARN(&emptyKmsKeyARN)
+					Expect(err).ToNot(HaveOccurred())
+				})
 			})
 		})
 
-		Context("when kmsKeyARN is not empty and matches the regex", func() {
-			BeforeEach(func() {
-				kmsKeyARN = "arn:aws:kms:us-east-1:111111111111:key/mrk-0123456789abcdef0123456789abcdef"
+		Context("kmsKeyARN regex", func() {
+			When("kmsKeyARN is not empty and matches the regex", func() {
+				It("should not return an error", func() {
+					validKmsKeyARN := "arn:aws:kms:us-east-1:111111111111:key/mrk-0123456789abcdef0123456789abcdef"
+					err := ValidateKMSKeyARN(&validKmsKeyARN)
+					Expect(err).ToNot(HaveOccurred())
+				})
 			})
 
-			It("should not return an error", func() {
-				err := ValidateKMSKeyARN(&kmsKeyARN)
-				Expect(err).ToNot(HaveOccurred())
-			})
-		})
-
-		Context("when kmsKeyARN is not empty and does not match the regex", func() {
-			BeforeEach(func() {
-				kmsKeyARN = "invalid-kms-key-arn"
+			When("kmsKeyARN is not empty but is not prefixed with 'mrk'", func() {
+				It("should return an error", func() {
+					invalidKmsKeyARN := "arn:aws:notkms:us-west-2:301721915996:key/9fdfaf2f-efb7-4db7-a5c3-0d047c52f094"
+					err := ValidateKMSKeyARN(&invalidKmsKeyARN)
+					Expect(err).To(HaveOccurred())
+				})
 			})
 
-			It("should return an error", func() {
-				err := ValidateKMSKeyARN(&kmsKeyARN)
-				Expect(err).To(HaveOccurred())
+			When("when kmsKeyARN is not empty and does not match the regex", func() {
+				It("should return an error", func() {
+					invalidKmsKeyARN := "invalid-kms-key-arn"
+					err := ValidateKMSKeyARN(&invalidKmsKeyARN)
+					Expect(err).To(HaveOccurred())
+				})
 			})
 		})
 	})

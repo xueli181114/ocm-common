b4973ea40c64ed358f4c48c0f6f0ad31a02abc53
David Lee <dle@redhat.com>
2024-06-27 17:10:54 -0400

    OCM-9166 | fix: create common validator to check duplicated IAM role arns for ROSA STS cluster

---
+++ pkg/rosa/operatorroles/operatorroles_suite_test.go
@@ -0,0 +1,13 @@
+package operatorroles_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestOperatorroles(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Operatorroles Suite")
+}
